4cab594e7e3d24e04992db6c322ef79b757492f9
Jordan <jordandoan@hotmail.com>
2020-03-02 09:17:55 -0800

    Init py done!

--- init.py
+++ init.py
@@ -1,5 +1,15 @@
 from decouple import config
+import requests
 init = "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/"
+key = config('API')
 map = open('map.txt', 'w')
 rooms = open('rooms.txt', 'w')
-key = config('API')
+headers = {'Authorization': 'Token ' + key}
+r = requests.get(init, headers=headers)
+data = r.json()
+traversal = {data['room_id']: {}}
+room = {data['room_id']: [data['coordinates'], data['title']]}
+for exit in data['exits']:
+    traversal[data['room_id']][exit] = '?'
+map.write(str(traversal))
+rooms.write(str(room))
--- traversal.py
+++ traversal.py
@@ -3,4 +3,4 @@ baseURL = "https://lambda-treasure-hunt.herokuapp.com/api/adv/"
 init = "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/"
 key = config('')
 while True:
-    r = requests.get(url=baseURL + "/"
+    r = requests.get(url=baseURL + "/")
