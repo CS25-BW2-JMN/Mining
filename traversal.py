828f7871f6a9565a04a0643c0ab6a310a32e34a3
Jordan <jordandoan@hotmail.com>
2020-03-02 09:43:24 -0800

    Traversal works with input, also accounts for cooldown wait

--- traversal.py
+++ traversal.py
@@ -2,5 +2,6 @@
 baseURL = "https://lambda-treasure-hunt.herokuapp.com/api/adv/"
 init = "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/"
 key = config('')
+
 while True:
     r = requests.get(url=baseURL + "/")
